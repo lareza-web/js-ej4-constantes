bc8c4894673e1506b1c36604f048189d0e2895fd
lareaza-web <larezaweb@gmail.com>
2020-05-18 14:27:07 -0300

    constantes

---
+++ constantes.js
@@ -0,0 +1,9 @@
+'use strict'
+//constante
+//Como una variable pero su valor no cambia
+
+var web = "http://demos-alarez.000webhostapp.com/";
+const ip = "192.168.12.5";
+
+web = "www.udemy.com/";
+console.log(web);
